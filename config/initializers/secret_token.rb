93393f1779bf435782b0775ccb34aafeddab9c65
itk <iitsuka.t@gmail.com>
2011-04-10 21:47:58 +0900

    inital commit but blank

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sharedvison::Application.config.secret_token = '12f248f2747a3ec86982a464c222c9ef04194d4983b70ce5a62fc6debcb5f07b249538fb4434a21c4b53137790af557b4d8e580411bf992f584d48daa322c3e4'
