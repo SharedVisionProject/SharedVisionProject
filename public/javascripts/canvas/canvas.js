1794d280a1fc962efb586de3a7d09ce3df299ab4
foxofzzz <foxofoxof@gmail.com>
2011-05-12 19:04:00 +0900

    Issues #11 文字列の描画

--- public/javascripts/canvas/canvas.js
+++ public/javascripts/canvas/canvas.js
@@ -4,12 +4,12 @@
 $(document).ready(drawAllPieces());
 
 function drawAllPieces(){
-	
+
 	$.getJSON("/pieces",null,function(data,status){
 		$.each(data,function(index,item){
 			drawPiece(item.piece);
 		})
-	})	
+	})
 }
 
 
@@ -24,9 +24,12 @@ function drawPiece(piece){
 		case "line":
 			drawLinePiece(piece,getActiveCanvas());
 			break;
+		case "text":
+			drawTextPiece(piece,getActiveCanvas());
+			break;
 		default:
 			break;
-	}	
+	}
 }
 
 function drawLinePiece(p,canv){
@@ -37,6 +40,23 @@ function drawLinePiece(p,canv){
 	ctx.moveTo(p.pos_x,p.pos_y);
 	ctx.lineTo(detail.end_x,detail.end_y);
 	ctx.closePath();
-	
+
 	ctx.stroke();
+}
+
+function drawTextPiece(p,canv){
+	var ctx = canv.getContext('2d');
+	var detail = eval("("+p.data+")");
+
+	if( detail.shadow != null)
+	{
+		ctx.shadowColor = detail.shadow.color;
+		ctx.shadowBlur = detail.shadow.blur;
+		ctx.shadowOffsetX = detail.shadow.offset_X;
+		ctx.shadowOffsetY = detail.shadow.offset_Y;
+	}
+
+	ctx.fillStyle  = detail.color;
+	ctx.font = detail.font;
+	ctx.fillText(detail.strings, p.pos_x, p.pos_y, null);
 }
